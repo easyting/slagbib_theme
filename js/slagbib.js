ed6fdced34855259a2aeac41b939214cc8e3fb46
Anna Caraman <ac@inlead.dk>
2012-10-23 11:45:33 +0000

    case 1394 - User login is open by default, logo is moved in the footer.

--- js/slagbib.js
+++ js/slagbib.js
@@ -1,8 +1,7 @@
 (function ($) {
   $(document).ready(function() {
-    //Sets attributes to commune logo
-    $('.logo-img a').attr('href', 'http://www.slagelse.dk/');
-    $('.logo-img a').attr('target', '_blank');
+    //Moves commune logo to footer
+    $('.region-footer-second .block-library-address .content > .address').append('<div class="logo-img"><a href="http://www.slagelse.dk/" rel="home" title="Slagelse bibliotekerne" class="active" target="_blank"><img src="/profiles/artesis/themes/slagbib_theme/logo.png" alt="Slagelse bibliotekerne" id="logo"></a></div>');
     $('.form-item-search-block-form > .ui-autocomplete-input').attr('placeholder', Drupal.t("Search materials, issues and more."));
   });
 })(jQuery);
